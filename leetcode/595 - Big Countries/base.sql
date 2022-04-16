4c0500c05800be37c9e2b9239dd4001032969bcd
ckgresla <ckgresla@gmail.com>
2022-04-16 15:41:40 -0500

    more leetcode

---
+++ leetcode/595 - Big Countries/base.sql
@@ -0,0 +1,11 @@
+/*SQL Comments Look like this*/ 
+/*I kinda knew that but just want to reiterate*/ 
+/*Original Problem- https://leetcode.com/problems/big-countries/ */ 
+
+
+SELECT name, population, area
+FROM World 
+WHERE area >= 3000000 OR population >= 25000000
+
+
+
