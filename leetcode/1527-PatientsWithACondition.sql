fd7e14bc2a02427e2c4bbd91d6e13c25595cccbb
ckgresla <ckgresla@gmail.com>
2022-04-28 15:59:38 -0500

    more SQL, how does one determine what they want to do in life?

--- leetcode/1527-PatientsWithACondition.sql
+++ leetcode/1527-PatientsWithACondition.sql
@@ -4,5 +4,7 @@
 SELECT patient_id, patient_name, conditions
 FROM Patients
 WHERE conditions LIKE '% DIAB1%' OR conditions LIKE 'DIAB1%'
+					  -- 1st condition finds all occurences of DIAB1 that occur after the first token in the "conditions" string
+					  -- 2nd Condition finds all the instances where the first item in the string is a DIAB1 (starts w this medical condition)
 
 
